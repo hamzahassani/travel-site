8ad28861f56eee308458f16af0fddf846a732dc3
Your Name <you@example.com>
2017-07-18 17:24:51 +0100

    bem

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp"),
   watch = require("gulp-watch"),
-  postcss = require("gulp-postcss");
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import");
 
 gulp.task("default", function() {
   console.log("huuu bravo");
@@ -13,7 +17,8 @@ gulp.task("html", function() {
 gulp.task("styles", function() {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(gulp.dest("./temp/styles"));
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest("./app/temp/styles"));
 });
 
 gulp.task("watch", function() {
